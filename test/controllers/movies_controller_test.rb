c562b7052196b19f67c873d750475a6a494e4704
Kiyu <qconnection@gmail.com>
2015-01-20 13:41:27 +0100

    Add test fixtures for actors, movies, and games

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class MoviesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@first_game = games(:first_game)
+		@existing_actor = actors(:Pacino)
+	end
+
+  test "get movie in game round" do
+  	get edit_game_movie_path(@first_game.id, movies(:Heat).id)
+  	assert_response :success
+  end
 end
--- test/integration/game_round_test.rb
+++ test/integration/game_round_test.rb
@@ -16,38 +16,51 @@ class GameRoundTest < ActionDispatch::IntegrationTest
 
   test "Enter actor name already in db" do
     assert_no_difference "Actor.count" do
-      post games_path, { :actor_name => @existing_actor,
-                         :players    => @one_valid_name }
+      post games_path, { actor_name: @existing_actor,
+                         players: @one_valid_name }
 
     end
+  end
+
+  test "fixture databases" do
+    assert_equal actors(:Pacino).movies.count, games(:first_game).movies.count
   end
 
   test "create a game session" do
   	get new_game_path
   	assert_response :success
   	assert_difference ["Game.count", "Player.count"], 1 do
-  		post games_path, { "actor_name" => @existing_actor,
+  		post games_path, { actor_name: @existing_actor,
   													players: @one_valid_name }
+      follow_redirect!
   	end
-    follow_redirect!
+
     assert_template 'edit'
     assert_select "form input"
   end
 
-  # test "succesfully play one round with existing actor name" do
-  # 	get new_game_path
-  # 	assert_response :success
-  # 	assert_difference ["Game.count", "Player.count"], 1 do
-  # 		post game_path, game_form: {"actor_name" => @existing_actor,
-  # 													player_names: @one_valid_name}
-  # 		assert_redirected_to edit_game_path
-  # 	  follow_redirect!
-  # 	end
-  # 	assert_select @movie.overview
-  # 	assert_select @player.id
-  # 	assert_select @player.name
-  # 	patch game_path(session[:id]), players_guesses: { "player[1]" => "42"}
-  # 	assert_response :success
-  # 	get edit_game_path
+  # test "what is the game id?" do
+  #   assert_difference "Game.last.id", 1 do
+  #     post games_path, { actor_name: @existing_actor,
+  #                           players: @one_valid_name }
+  #   end
   # end
+
+  test "succesfully play one round with existing actor name" do
+  	get new_game_path
+  	assert_response :success
+  	assert_difference ["Game.count", "Player.count"], 1 do
+  		post games_path, { actor_name: @existing_actor,
+  													players: @one_valid_name }
+  		# assert_redirected_to edit_game_path
+      # follow_redirect!
+  	end
+  	# assert_select @movie.overview
+  	# assert_select @player.id
+  	# assert_select @player.name
+  	patch game_movie_path("2", "11162")
+    assert_response :success
+  	get edit_game_path
+  end
+
 end
